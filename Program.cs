2aee1e43a9593a242bdf7919f7465bda8cad429c
Aldoshin_Alexandr <aldoshin_a@internet.ru>
2023-01-17 22:52:15 +0300

    Решение задачи №4

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N > 0)
+{
+    while (N >= 2)
+    {
+        if (N > 0 && N % 2 == 0)
+        {
+            Console.WriteLine(N);
+            N = N - 1;
+        }
+        else
+        {
+            N = N - 1;
+        }
+    }
+}
+else if (N < 0)
+{
+    while (N <= -2)
+    {
+        if (N < 0 && N % 2 == 0)
+        {
+            Console.WriteLine(N);
+            N = N + 1;
+        }
+        else
+        {
+            N = N + 1;
+        }
+    }
+}
+else if (N == 0)
+{ Console.WriteLine("Введено нулевое значение"); }
